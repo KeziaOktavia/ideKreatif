419cea216acec16fa4316a1f67582b0fd9b8b124
keyjiaa <keziatavia03@gmail.com>
2025-01-19 16:31:26 +0700

    login_auth.php

--- auth/login_auth.php
+++ auth/login_auth.php
@@ -32,5 +32,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 'message' => 'Username atau Password salah'
             ];
         }
+    } else {
+        // Username tidak ditemukan
+        $_SESSION['notification'] = [
+            'type' => 'danger',
+            'message' => 'Username atau Password salah'
+        ];
     }
+    // Redirect kembali ke halaman login jika gagal
+    header('Location: login_php');
+    exit();
 }
+$conn->close();
+?>
