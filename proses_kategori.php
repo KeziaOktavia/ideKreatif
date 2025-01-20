30501c7d24d6fa2f583ee5427c6a1c9b166f6b50
keyjiaa <keziatavia03@gmail.com>
2025-01-20 11:56:14 +0700

    proses_kategori.php

---
+++ proses_kategori.php
@@ -0,0 +1,60 @@
+<?php
+
+// Menghubungkan ke file konfigurasi database
+include("config.php")
+
+// Memulai sesi untuk menyimpan notifikasi
+session_start();
+
+//Proses penambahankategori baru 
+if (isset($_POST['simpan'])) {
+    // Mengambil data nama kategori dari form
+    $category_name = $_POST['category_name'];
+
+    // Query untuk menambahkan data kategori ke dalam database
+    $query= "INSERT INTO categories (category_name) VALUES ('$category_name')";
+    $exec = mysqli_query($conn, $query);
+
+    // Menyimpan notifikasi berhasil atau gagal ke dalam session
+    if ($exec) {
+        $_SESSION['notification'] =[
+            'type' => 'primary', // Jenis notifikasi (contoh: primary untuk keberhasilan)
+            'message' => 'Kategori berhasil ditambahkan!'
+        ];
+    } else {
+        $_SESSION['notification'] = [
+            'type' => 'danger' //Jenis notifikasi (contoh: danger untuk kegagalan)
+            'message' => 'Gagal menambahkan kategori: ' . mysqli_error($conn)
+        ];
+    }
+
+    // Redirect  kembali ke halaman kategori
+    header('Location: kategori.php');
+    exit();
+}
+
+// Proses penghapusan kategori
+if (isset($_POST['delete'])) {
+    // Mengambil Id kategori dari parameter ID
+    $catID = $_POST['catID'];
+
+    // Query untuk menghapus kategori berdasarkan ID
+    $exec = mysqli_query($conn, "DELETE FROM categories WHERE category_id='$catID'");
+
+    // Menyimpan notifikasi keberhasilan atau kegagalan ke dalam session
+    if ($exec) {
+        $_SESSION['notification'] = [
+            'type' => 'primary',
+            'message' => 'Kategori berhasil dihapus!'
+        ];
+    } else {
+        $_SESSION['notification'] = [
+            'type' => 'danger',
+            'message' => 'Gagal menghapus kategori: ' . mysqli_error($conn)
+        ];
+    }
+
+// Redicert kembali ke halaman kategori
+header('Location: kategori.php');
+exit();
+}
