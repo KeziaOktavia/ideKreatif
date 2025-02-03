b32a8c03169afca8d9ae8c12078aea15ad40c283
keyjiaa <keziatavia03@gmail.com>
2025-02-03 10:11:04 +0700

    init_session.php

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
 
+// Periksa apakah kunci "name" dan "role" ada dalam sesi sebelum mengaksesnya
 $name = $_SESSION["name"];
 $role = $_SESSION["role"];
+
 // Ambil notifikasi jika ada, kemudian hapus dari sesi
 $notification = $_SESSION['notification'] ?? null;
 if ($notification) {
